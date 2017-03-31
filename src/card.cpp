820ac2cc1c7d8d83e4723a05b19ec94e67386b4e
Thomas Kremel <kremelt@gmail.com>
2017-03-31 23:27:26 +0200

    [card] added functionality

---
+++ src/card.cpp
@@ -0,0 +1,207 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Thomas Kremel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <exception>
+#include <string>
+#include <vector>
+#include "src/card.hpp"
+#include "src/database.hpp"
+
+namespace magicSearchEngine {
+
+    /*
+     * Some of these can be traits-templated, maybe in the future.
+     */
+    layout_t
+    Card::parse_layout(const card_t & card) {
+        auto && layouts = db->get_layout();
+        try {
+            if (card.find("layout") != card.end())
+                return &(layouts.at(card["layout"]));
+        } catch (const std::out_of_range &) {
+            std::string msg = "Layout of " + name +
+                    " does not refer to any layout in the database, check rules.";
+            throw std::out_of_range(msg);
+        }
+        return nullptr;
+    }
+
+    names_t
+    Card::parse_names(const card_t & card) {
+        std::vector<std::string> names;
+        if (card.find("names") != card.end()) {
+            for (auto && name : card["names"])
+                names.push_back(name);
+        }
+        return std::move(names);
+    }
+
+    manaCost_t
+    Card::parse_manaCost(const card_t & card) {
+        throw std::exception();
+    }
+
+    colors_t
+    Card::parse_colors(const card_t & card) {
+        auto && db_colors = db->get_colors();
+        colors_t card_colors;
+        try {
+            if (card.find("colors") != card.end()) {
+                for (auto && color : card["colors"])
+                    card_colors.push_back(&(db_colors.at(color)));
+            }
+        } catch (const std::out_of_range &) {
+            std::string msg = "One of colors of " + name +
+                    " does not refer to any color in the database, check rules.";
+            throw std::out_of_range(msg);
+        }
+        return std::move(card_colors);
+    }
+
+    supertypes_t
+    Card::parse_supertypes(const card_t & card) {
+        auto && db_supertypes = db->get_supertypes();
+        supertypes_t card_supertypes;
+        try {
+            if (card.find("supertypes") != card.end()) {
+                for (auto && supertype : card["supertypes"])
+                    card_supertypes.push_back(&(db_supertypes.at(supertype)));
+            }
+        } catch (const std::out_of_range &) {
+            std::string msg = "One of supertypes of " + name +
+                    " does not refer to any supertype in the database, check rules.";
+            throw std::out_of_range(msg);
+        }
+        return std::move(card_supertypes);
+    }
+
+    types_t
+    Card::parse_types(const card_t & card) {
+        auto && db_types = db->get_types();
+        types_t card_types;
+        try {
+            if (card.find("types") != card.end()) {
+                for (auto && type : card["types"])
+                    card_types.push_back(&(db_types.at(type)));
+            }
+        } catch (const std::out_of_range &) {
+            std::string msg = "One of types of " + name +
+                    " does not refer to any type in the database, check rules.";
+            throw std::out_of_range(msg);
+        }
+        return std::move(card_types);
+    }
+
+    subtypes_t
+    Card::parse_subtypes(const card_t & card) {
+        auto && db_subtypes = db->get_subtypes();
+        subtypes_t card_subtypes;
+        try {
+            if (card.find("subtypes") != card.end()) {
+                for (auto && subtype : card["subtypes"])
+                    card_subtypes.push_back(&(db_subtypes.at(subtype)));
+            }
+        } catch (const std::out_of_range &) {
+            std::string msg = "One of subtypes of " + name +
+                    " does not refer to any subtype in the database, check rules.";
+            throw std::out_of_range(msg);
+        }
+        return std::move(card_subtypes);
+    }
+
+    /*
+     * Only getters follows.
+     */
+
+    layout_t
+    Card::get_layout() const {
+        return layout;
+    }
+
+    name_t
+    Card::get_name() const {
+        return name;
+    }
+
+    names_t
+    Card::get_names() const {
+        return names;
+    }
+
+    manaCost_t
+    Card::get_manaCost() const {
+        return manaCost;
+    }
+
+    colors_t
+    Card::get_colors() const {
+        return colors;
+    }
+
+    supertypes_t
+    Card::get_supertypes() const {
+        return supertypes;
+    }
+
+    types_t
+    Card::get_types() const {
+        return types;
+    }
+
+    subtypes_t
+    Card::get_subtypes() const {
+        return subtypes;
+    }
+
+    text_t
+    Card::get_text() const {
+        return text;
+    }
+
+    power_t
+    Card::get_power() const {
+        return power;
+    }
+
+    toughness_t
+    Card::get_toughness() const {
+        return toughness;
+    }
+
+    loyalty_t
+    Card::get_loyalty() const {
+        return loyalty;
+    }
+
+    hand_t
+    Card::get_hand() const {
+        return hand;
+    }
+
+    life_t
+    Card::get_life() const {
+        return life;
+    }
+}
+
--- src/card.hpp
+++ src/card.hpp
@@ -1,12 +1,30 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * MIT License
+ *
+ * Copyright (c) 2017 Thomas Kremel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 /* 
- * File:   Card.hpp
- * Author: thomas
+ * File:   card.hpp
+ * Author: Thomas Kremel
  *
  * Created on 24 March 2017, 14:35
  */
@@ -15,34 +33,111 @@
 #define CARD_HPP
 
 #include <string>
+#include <vector>
+#include <limits.h>
+#include "src/json.hpp"
 
-namespace MagicSearchEngine {
-    using id_t = std::string;
-    using layout_t = std::string;
-    using rarity_t = std::string;
+namespace magicSearchEngine {
+
+    using layout_t = std::string *;
+    using name_t = std::string;
+    using names_t = std::vector<std::string>;
+    using manaCost_t = std::vector<std::string>; // TODO
+    using colors_t = std::vector<std::string *>;
+    using supertypes_t = std::vector<std::string *>;
+    using types_t = std::vector<std::string *>;
+    using subtypes_t = std::vector<std::string *>;
+    using text_t = std::string;
+    using power_t = int;
+    using toughness_t = int;
+    using loyalty_t = int;
+    using hand_t = int;
+    using life_t = int;
+
+    using card_t = const nlohmann::basic_json<std::map, std::vector,
+            std::__cxx11::basic_string<char, std::char_traits<char>,
+            std::allocator<char> >, bool, long, unsigned long, double,
+            std::allocator, nlohmann::adl_serializer> &;
+
+    // Forward declaration, avoiding cycle dependencies.
+    class Database;
 
     class Card {
     private:
-        id_t id;
+        Database * db;
         layout_t layout;
-        rarity_t rarity;
+        name_t name;
+        names_t names;
+        manaCost_t manaCost;
+        colors_t colors;
+        supertypes_t supertypes;
+        types_t types;
+        subtypes_t subtypes;
+        text_t text;
+        power_t power;
+        toughness_t toughness;
+        loyalty_t loyalty;
+        hand_t hand;
+        life_t life;
+
     public:
+        // After quite long time spent by trying to solve this nicely, 
+        // I decided for a macro. #sorryjako
+#define SET(attr, fail) if (card.find("attr") != card.end()) attr = card["attr"]; else attr = fail
 
-        Card(id_t id_, layout_t layout_, rarity_t rarity_) :
-        id(id_), layout(layout_), rarity(rarity_) {
+        Card(const card_t & card, Database * dat) : db(dat) {
+            SET(name, "");
+            SET(text, "");
+            SET(power, INT_MIN);
+            SET(toughness, INT_MIN);
+            SET(loyalty, INT_MIN);
+            SET(hand, INT_MIN);
+            SET(life, INT_MIN);
+            layout = parse_layout(card);
+            names = parse_names(card);
+            manaCost = parse_manaCost(card);
+            colors = parse_colors(card);
+            supertypes = parse_supertypes(card);
+            types = parse_types(card);
+            subtypes = parse_subtypes(card);
         }
 
-        id_t get_id() {
-            return id;
-        }
-        
-        layout_t get_layout() {
-            return layout;
-        }
-        
-        rarity_t get_rarity(){
-            return rarity;
-        }
+        layout_t get_layout() const;
+        name_t get_name() const;
+        names_t get_names() const;
+        manaCost_t get_manaCost() const;
+        colors_t get_colors() const;
+        supertypes_t get_supertypes() const;
+        types_t get_types() const;
+        subtypes_t get_subtypes() const;
+        text_t get_text() const;
+        power_t get_power() const;
+        toughness_t get_toughness() const;
+        loyalty_t get_loyalty() const;
+        hand_t get_hand() const;
+        life_t get_life() const;
+
+    private:
+        layout_t
+        parse_layout(const card_t & card);
+
+        names_t
+        parse_names(const card_t & card);
+
+        manaCost_t
+        parse_manaCost(const card_t & card);
+
+        colors_t
+        parse_colors(const card_t & card);
+
+        supertypes_t
+        parse_supertypes(const card_t & card);
+
+        types_t
+        parse_types(const card_t & card);
+
+        subtypes_t
+        parse_subtypes(const card_t & card);
     };
 }
 #endif /* CARD_HPP */
